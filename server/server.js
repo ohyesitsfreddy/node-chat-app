9ebf8c954c4f668eded0e5a1f896f03aece404bc
Frederik Nebas <frederiknebas@FN-MacBook-Pro-2016.local>
2018-07-02 16:13:56 +0200

    Add new things

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 // dont define port hardcoded for heroku upload:
 const port = process.env.PORT || 3000;
@@ -7,10 +9,23 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
