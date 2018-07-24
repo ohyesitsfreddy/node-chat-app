77522e124060a43269bb0aa9c1240b17bfd47d6e
Frederik Nebas <frederiknebas@FN-MacBook-Pro-2016.local>
2018-07-24 13:17:50 +0200

    New User and alert

--- server/server.js
+++ server/server.js
@@ -19,6 +19,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    })
     // socket.emit('newEmail', {
     //     from: 'mike@example.com',
     //     text: 'Hey, what is going on.',
@@ -33,11 +44,16 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        });
+        })
     });
 
     // socket.on('createEmail', (newEmail) => {
